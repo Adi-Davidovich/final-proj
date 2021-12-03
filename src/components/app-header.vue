a7d9650c3fd01c5b6a6f6550d358e029c8b57499
Sahar zigdon <saharzigdon123@gmail.com>
2021-12-03 18:24:21 +0200

    12:00

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,40 +1,42 @@
 
 <template>
-<div class="main-layout">
+  <header class="main-header main-layout">
+    <div class="header-container">
+      <div class="logo">Fiverr</div>
+      <nav class="main-nav">
+        <router-link to="/explore" class="link">Explore</router-link>
+        <router-link to="/start_selling" class="link"
+          >Become a seller</router-link
+        >
+        <router-link to="/login" class="link">Sign In</router-link>
+        <router-link to="/login" class="link link-join">Join</router-link>
+      </nav>
+    </div>
+  </header>
 
-<header class="main-header">
-  <div class="logo">Fiverr</div>
-  <nav class="main-nav">
-    <router-link to="/explore" class="link">Explore</router-link>
-    <router-link to="/start_selling" class="link">Become a seller</router-link>
-    <router-link to="/login" class="link">Sign In</router-link>
-    <router-link to="/login" class="link link-join">Join</router-link>
-  </nav>
-</header>
-</div>
   <!-- <div class="main-header-sticky main-layout"> -->
-    <!-- <div class="user-msg"></div> -->
-    <!-- <div class="header">
+  <!-- <div class="user-msg"></div> -->
+  <!-- <div class="header">
       <header class="logged-out-homepage-header header-transparent">
         <router-link class="logo" to="/" exact
           >fiverr<span class="logo-dot">.</span></router-link
         > -->
 
-        <!-- <div class="fiver-header-search-animated">
+  <!-- <div class="fiver-header-search-animated">
               <div class="search-bar-package search_bar-package">
                 <form class=""><button>Search</button></form>
               </div>
               </div> -->
-        <!-- <nav>
+  <!-- <nav>
           <router-link v-if="user" :to="'/user/' + user._id">Hi {{ user.fullname }}</router-link>
           <router-link to="/explore">Explore</router-link> | -->
-          <!-- <router-link to="/dashboard">Dashboard</router-link> |
+  <!-- <router-link to="/dashboard">Dashboard</router-link> |
                 <router-link to="/review">Reviews</router-link>|
                 <router-link to="/chat">Chat</router-link>| -->
-          <!-- <router-link to="/start_selling">Become a Seller</router-link>|
+  <!-- <router-link to="/start_selling">Become a Seller</router-link>|
           <router-link to="/login">Sign In</router-link> -->
-          <!-- <router-link to="/about">About</router-link> -->
-          <!-- <button>Join</button>
+  <!-- <router-link to="/about">About</router-link> -->
+  <!-- <button>Join</button>
         </nav>
       </header>
     </div>
