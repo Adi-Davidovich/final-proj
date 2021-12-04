2325d408d3781029ea2db111083d926dc5ecba9f
Sahar zigdon <saharzigdon123@gmail.com>
2021-12-04 12:14:51 +0200

    asdsa

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -38,40 +38,56 @@
     </div>
 
     <div class="homePage-info-container main-layout">
-      <div class="homePage-info">
-        <h2>A whole world of freelance talent at your fingertips</h2>
-        <ul>
-          <li>
-            <h6><span><i class="far fa-check-circle"></i></span>The best for every budget</h6>
-            <p>
-              Find high-quality services at every price point. No hourly rates,
-              just project-based pricing.
-            </p>
-          </li>
+      <div class="main-info flex">
+        <div class="homePage-info">
+          <h2>A whole world of freelance talent at your fingertips</h2>
+          <ul>
+            <li>
+              <h6>
+                <span><i class="far fa-check-circle"></i></span>The best for
+                every budget
+              </h6>
+              <p>
+                Find high-quality services at every price point. No hourly
+                rates, just project-based pricing.
+              </p>
+            </li>
 
-          <li>
-            <h6><span><i class="far fa-check-circle"></i></span>Quality work done quickly</h6>
-            <p>
-             Find the right freelancer to begin working on your project within minutes.
-            </p>
-          </li>
+            <li>
+              <h6>
+                <span><i class="far fa-check-circle"></i></span>Quality work
+                done quickly
+              </h6>
+              <p>
+                Find the right freelancer to begin working on your project
+                within minutes.
+              </p>
+            </li>
 
-          <li>
-            <h6><span><i class="far fa-check-circle"></i></span>Protected payments, every time</h6>
-            <p>
-              Always know what you'll pay upfront. Your payment isn't released until you approve the work.
-            </p>
-          </li>
-          <li>
-            <h6><span><i class="far fa-check-circle"></i></span>24/7 support</h6>
-            <p>
-            Questions? Our round-the-clock support team is available to help anytime, anywhere.
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div class="homePage-img">
-        <img src="../assets/img/selling-proposition.png" alt="">
+            <li>
+              <h6>
+                <span><i class="far fa-check-circle"></i></span>Protected
+                payments, every time
+              </h6>
+              <p>
+                Always know what you'll pay upfront. Your payment isn't released
+                until you approve the work.
+              </p>
+            </li>
+            <li>
+              <h6>
+                <span><i class="far fa-check-circle"></i></span>24/7 support
+              </h6>
+              <p>
+                Questions? Our round-the-clock support team is available to help
+                anytime, anywhere.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="homePage-img">
+          <img src="../assets/img/selling-proposition.png" alt="" />
+        </div>
       </div>
     </div>
   </section>
