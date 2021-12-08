aef897d940a4c8d9662d05a7b188ac75bf2cad65
Sahar zigdon <saharzigdon123@gmail.com>
2021-12-08 21:32:30 +0200

    sahar

--- src/views/gig-order.vue
+++ src/views/gig-order.vue
@@ -99,7 +99,7 @@ export default {
       return +(this.gig.price + this.serviceFee);
     },
     reviewsLength() {
-      return this.loggedInUser.reviews.length;
+      // return this.loggedInUser.reviews.length;
     },
   },
   watch: {
